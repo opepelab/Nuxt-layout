f605b93ea24ed9e7f620376af38eadf93a278a56
S.watana <75829584+opepelab@users.noreply.github.com>
2021-07-19 15:02:12 +0900

    Add files via upload

---
+++ default.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="resp">
+        <PictureHigh />
+		<Navbar />
+		<main>
+			<nuxt />
+		</main>
+		<Footer />
+	</div>
+</template>
+
+<script>
+    import PictureHigh from "/components/PictureHigh";
+	import Navbar from "/components/Navbar"; // headerをインポート
+	import Footer from "/components/Footer"; // footerをインポート
+
+	export default {
+		components: {
+            PictureHigh,
+			Navbar,
+			Footer,
+		}
+	};
+</script>
